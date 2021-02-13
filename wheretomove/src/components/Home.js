8ac9f3fe7c1da5ee9679cb1a4b6bff90a823d00a
Jeahong Hwang <95chris0319@gmail.com>
2021-02-13 18:11:23 +0900

    add ui components from material-ui/core

--- wheretomove/src/components/Home.js
+++ wheretomove/src/components/Home.js
@@ -1,9 +1,35 @@
 import React, {useState, useEffect} from 'react';
+import { Container, Typography, Link, ProTip, Button, Box } from '@material-ui/core';
+
+const Copyright = () => {
+	return (
+		<Typography variant="body2" color="textSecondary" align="center">
+			{'Copyright @ '}
+			<Link color="inherit" href="https://material-ui.com/">
+				Your Website
+			</Link>{' '}
+			{new Date().getFullYear()}
+			{'.'}
+		</Typography>
+	)
+}
 
 export const Home = () => {
 	return (
 		<>
-			<div>Hello</div>
+			<Container maxWidth='sm'>
+				<Box my={4}>
+					<Typography variant='h4' component="h1" gutterBottom>
+						AI Express
+					</Typography>
+					<Typography variant='body2' color="textSecondary">
+						Find out where to move in South Korea
+					</Typography>
+				</Box>
+				<Box>
+					
+				</Box>
+			</Container>
 		</>
 	)
 }
